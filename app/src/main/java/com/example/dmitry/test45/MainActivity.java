83e9f9213259f41f56892aee89b6101fc118c3ef
Dmitry Rybakov <drybakov@waverleysoftware.com>
2015-10-09 18:07:47 +0300

    Find the largest increasing sequence

--- app/src/main/java/com/example/dmitry/test45/MainActivity.java
+++ app/src/main/java/com/example/dmitry/test45/MainActivity.java
@@ -136,6 +136,85 @@ class LIST {
             }
         }
     }
+    
+    //////////// Heap sort //////////
+    
+    /// Find the largest increasing sequence n * log n//////
+    //    P = array of length N
+    //    M = array of length N + 1
+    //
+    //    L = 0
+    //            for i in range 0 to N-1:
+    //    // Binary search for the largest positive j ≤ L
+    //    // such that X[M[j]] < X[i]
+    //    lo = 1
+    //    hi = L
+    //    while lo ≤ hi:
+    //    mid = ceil((lo+hi)/2)
+    //            if X[M[mid]] < X[i]:
+    //    lo = mid+1
+    //            else:
+    //    hi = mid-1
+    //
+    //    // After searching, lo is 1 greater than the
+    //    // length of the longest prefix of X[i]
+    //    newL = lo
+    //
+    //    // The predecessor of X[i] is the last index of
+    //    // the subsequence of length newL-1
+    //    P[i] = M[newL-1]
+    //    M[newL] = i
+    //
+    //    if newL > L:
+    //    // If we found a subsequence longer than any we've
+    //    // found yet, update L
+    //    L = newL
+    //
+    //            // Reconstruct the longest increasing subsequence
+    //            S = array of length L
+    //            k = M[L]
+    //    for i in range L-1 to 0:
+    //    S[i] = X[k]
+    //    k = P[k]
+    //
+    //            return S
+    static int[] findLargestIncreasingSequence(int[] x) {
+        int p[] = new int[x.length];
+        int m[] = new int[x.length + 1];
+        int L = 0;
+        for(int i = 0; i < x.length; i++) {
+            // Binary search for the largest positive j ≤ L
+            // such that x[m[j]] < x[i]
+            int lo = 1;
+            int hi = L;
+            while (lo <= hi) {
+                int mid = lo +  (hi - lo) / 2;
+                if (x[m[mid]] < x[i]) {
+                    lo = mid + 1;
+                }
+                else {
+                    hi = mid - 1;
+                }
+            }
+            // The predecessor of X[i] is the last index of
+            // the subsequence of length newL-1
+            int newL = lo;
+            p[i] = m[newL - 1];
+            m[newL] = i;
+            // If we found a subsequence longer than any we've
+            // found yet, update L
+            if (newL > L) {
+                L = newL;
+            }
+        }
+        int s[] = new int[L];
+        int k = m[L];
+        for(int i = L-1; i >= 0; i--) {
+            s[i] = x[k];
+            k = p[k];
+        }
+        return s;
+    }
 };
 
 public class MainActivity extends AppCompatActivity {
